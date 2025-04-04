9b1f27995cf8de1dca383fd3b8b24c6b4bc5f660
DanielRP10 <danielr.202010@gmail.com>
2025-04-03 20:44:51 -0600

    Creación de entorno Flask
    Adjunto todo los archivo de la primer guía de falsk

---
+++ src/app.py
@@ -0,0 +1,33 @@
+from flask import Flask
+from flask_cors import CORS
+from config.config import app_config
+
+#Crear una instancia de la aplicacion flask 
+app = Flask(__name__)
+
+#Habilitar CORS para todas las rutas y origenes
+#Esto permite que el frontend realice solitudes a la API
+CORS(app)
+
+#Definir un manejador de errores para rutas no encontradas
+def paginaNoEncontrada(error):
+    return "<h1>Página no encontrada</h1>", 404
+
+def errorServidor(error):
+    return "<h1>Error intero del servidor</h1>", 500
+
+@app.route('/')
+def principal():
+    return "<h1>Bienvenido a mi aplicacion con Falsk</h1>"
+
+if __name__ == '__main__':
+    #Cargamos las configuracion para el entorno 'development'
+    app.config.from_object(app_config['development'])
+
+    #Registramos el manejador de error 404
+    app.register_error_handler(404, paginaNoEncontrada)
+    app.register_error_handler(500, errorServidor)
+
+    #Iniciamos el servidor de flask, escuchando en el puerto 5000.
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
